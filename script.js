a4b78f441e0fc4f1c8a3961c650d6c1d89efbd78
Nikhil6161 <nikhilappani6161@gmail.com>
2026-02-27 23:19:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(()=>{
+slides[index].classList.remove("active");
+index = (index + 1) % slides.length;
+slides[index].classList.add("active");
+},3000);
+
+function copyNumber(number){
+navigator.clipboard.writeText(number);
+
+document.getElementById("copyMsg").innerText="Number copied!";
+setTimeout(()=>{
+document.getElementById("copyMsg").innerText="";
+},2000);
+}
+
+/* ===== ROOM GALLERY SLIDER ===== */
+
+const galleryImages = document.querySelectorAll(".slider-card .room-image");
+const prevBtn = document.querySelector(".slider-card .left");
+const nextBtn = document.querySelector(".slider-card .right");
+
+let galleryIndex = 0; // ✅ separate variable
+
+function showImage(i){
+    galleryImages.forEach(img => img.classList.remove("active"));
+    galleryImages[i].classList.add("active");
+}
+
+nextBtn.addEventListener("click", ()=>{
+    galleryIndex++;
+    if(galleryIndex >= galleryImages.length) galleryIndex = 0;
+    showImage(galleryIndex);
+});
+
+prevBtn.addEventListener("click", ()=>{
+    galleryIndex--;
+    if(galleryIndex < 0) galleryIndex = galleryImages.length-1;
+    showImage(galleryIndex);
+});
+showImage(galleryIndex);
